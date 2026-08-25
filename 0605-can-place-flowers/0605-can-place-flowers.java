26e82fcfc49fc90c7b16267a50ce5d5b7bba4a61
Kanav Garg <149443901+kanav157@users.noreply.github.com>
2026-08-25 16:08:16 -0400

    Time: 1 ms (98.69%), Space: 47.9 MB (18.86%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        int a = 0;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+
+            if (flowerbed[i] == 0 &&
+                (i == 0 || flowerbed[i - 1] == 0) &&
+                (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+
+                flowerbed[i] = 1;
+                a = a + 1;
+            }
+        }
+
+        if (a >= n) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
