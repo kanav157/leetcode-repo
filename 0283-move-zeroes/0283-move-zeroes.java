809c2cae52434b946da13024a7eb74008a7979ae
Kanav Garg <149443901+kanav157@users.noreply.github.com>
2026-08-27 18:25:59 -0400

    Time: 2 ms (91.91%), Space: 47.4 MB (93.52%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int[] a = new int[nums.length];
+        int j = 0;
+        for (int i = 0 ; i<nums.length ; i++)
+        {
+            if (nums[i] != 0)
+            {
+                a[j] = nums[i];
+                j++;
+            }
+        }
+
+        for (int i = 0 ; i< nums.length;i++)
+        {
+            nums[i] = a[i];
+        }
+    }
+}
