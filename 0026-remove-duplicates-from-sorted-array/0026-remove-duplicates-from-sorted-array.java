887b893418d23965ab364ef447884a6ca8572e07
Kanav Garg <149443901+kanav157@users.noreply.github.com>
2026-09-07 01:01:37 -0400

    Time: 1 ms (77.24%), Space: 46.9 MB (24.16%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,31 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
 
-        int k = 0;
-        int[] kanav = new int[nums.length];
-
-        for (int i = 0; i < nums.length; i++) {
-
-            boolean found = false;
-
-            for (int j = 0; j < k; j++) {
-
-                if (nums[i] == kanav[j]) {
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                kanav[k] = nums[i];
+        int k = 1;
+        for (int i = 1 ; i < nums.length  ; i++)
+        {
+            if (nums[i] != nums[k-1])
+            {
+                nums[k] = nums[i];
                 k++;
             }
         }
-
-        for (int i = 0; i < k; i++) {
-            nums[i] = kanav[i];
-        }
-
         return k;
+
     }
 }
