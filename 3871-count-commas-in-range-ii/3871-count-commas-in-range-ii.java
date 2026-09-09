2c9cefd5470b61d4706cc9403b7ac340d8c100ae
Kanav Garg <149443901+kanav157@users.noreply.github.com>
2026-09-09 11:06:52 -0400

    Time: 1 ms (99.36%), Space: 42.8 MB (37.18%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long countCommas(long n) {
+        long answer = 0;
+        long start = 1000;
+        long commas = 1;
+
+        while (start <= n)
+        {
+            long end = Math.min(n,start*1000 - 1);
+            answer = answer + (end -start + 1)*commas;
+
+            start = start * 1000;
+            commas ++;
+
+        }
+        return answer;
+    }
+}
