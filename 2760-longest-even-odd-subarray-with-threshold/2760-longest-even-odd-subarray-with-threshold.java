1f3a07d61e1ad2492ff7d6b18e3e20c1d8e29fac
Kanav Garg <149443901+kanav157@users.noreply.github.com>
2026-09-13 16:54:49 -0400

    Time: 5 ms (41.74%), Space: 47.1 MB (10.12%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
@@ -1,47 +1,42 @@
-class Solution {
+class Solution { 
+ 
+    public int longestAlternatingSubarray(int[] nums, int threshold) { 
+ 
+        int count = 0; 
+ 
+        for (int i = 0; i < nums.length; i++) { 
+            if (nums[i] % 2 != 0) { 
+                count++; 
+            } 
+        } 
+ 
+        if (count == nums.length) { 
+            return 0; 
+        } 
+ 
+        int i = 0; 
+        int ans = 0; 
+ 
+        for (int j = 0; j < nums.length; j++) { 
 
-    public int longestAlternatingSubarray(int[] nums, int threshold) {
-
-        int count = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 != 0) {
-                count++;
-            }
-        }
-
-        if (count == nums.length) {
-            return 0;
-        }
-
-        int i = 0;
-        int ans = 0;
-
-        for (int j = 0; j < nums.length; j++) {
-
-            // If current number is greater than threshold,
-            // start a new subarray after j
             if (nums[j] > threshold) {
                 i = j + 1;
                 continue;
             }
-
-            // Move i until we find an even number
-            while (i <= j && nums[i] % 2 != 0) {
-                i++;
-            }
-
-            // If two consecutive numbers have the same parity,
-            // start from the current number
-            if (j > i && nums[j] % 2 == nums[j - 1] % 2) {
-                i = j;
-            }
-
-            if (i <= j && nums[i] % 2 == 0) {
-                ans = Math.max(ans, j - i + 1);
-            }
-        }
-
-        return ans;
-    }
+ 
+            while (i <= j && nums[i] % 2 != 0) { 
+                i++; 
+            } 
+ 
+            if (j > i && nums[j] % 2 == nums[j - 1] % 2) { 
+                i = j; 
+            } 
+ 
+            if (i <= j && nums[i] % 2 == 0) { 
+                ans = Math.max(ans, j - i + 1); 
+            } 
+        } 
+ 
+        return ans; 
+    } 
 }
